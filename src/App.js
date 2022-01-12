7b898fb257dc815e42bd39231c5f5aeee665b642
sruthi-lal <lalsruthi145@gmail.com>
2022-01-12 11:02:10 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import unsplash from './api/unsplash'
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
+
+
+
+class App extends React.Component {
+  state = { images : [] };
+   onSearchSubmit = async term => {
+ const response = await unsplash.get('/search/photos',{
+     params: {query: term },
+     
+
+   }); 
+    console.log(this)
+   this.setState({images : response.data.results})
+  }
+  render() {
+    return (
+      <div className='ui container' style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+       <ImageList images={this.state.images} />
+      </div>
+    );
+  }
+}
+
+export default App;
